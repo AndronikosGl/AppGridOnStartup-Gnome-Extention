8c47bab2a2e061219711c45da181de9a954456d2
AndronikosGl <athem200@gmail.com>
2026-01-22 00:55:28 +0200

    Add files via upload

--- extension.js
+++ extension.js
@@ -1,29 +1,35 @@
 import Gio from 'gi://Gio';
-    
-    import * as Main from 'resource:///org/gnome/shell/ui/main.js';
-    
-    export default class AppGridOnStartup {
-    
-        enable() {
-            if (!Main.layoutManager._startingUp) return;
-        
-            this._settings = new Gio.Settings({ schema: 'org.gnome.desktop.interface' });
-            
-            Main.layoutManager.connectObject('startup-complete', () => {
-                Main.layoutManager.disconnect(signalId);
-                this.#openAppGrid();
-            }, this);
-        }
-        
-        disable() {
-            this._settings = null;
-            Main.layoutManager.disconnectObject(this);
-        }
-    
-        #openAppGrid() {
-            const prev = this._settings.get_boolean('enable-animations');
-            this._settings.set_boolean('enable-animations', false);
-            Main.overview.showApps();
-            this._settings.set_boolean('enable-animations', prev);
-        }
+import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+
+export default class AppGridOnStartup {
+
+    enable() {
+        if (!Main.layoutManager._startingUp)
+            return;
+
+        this._settings = new Gio.Settings({
+            schema: 'org.gnome.desktop.interface',
+        });
+
+        Main.layoutManager.connectObject(
+            'startup-complete',
+            () => this.#openAppGrid(),
+            this
+        );
     }
+
+    disable() {
+        this._settings = null;
+        Main.layoutManager.disconnectObject(this);
+    }
+
+    #openAppGrid() {
+        const prev = this._settings.get_boolean('enable-animations');
+        this._settings.set_boolean('enable-animations', false);
+
+        Main.overview.showApps();
+
+        this._settings.set_boolean('enable-animations', prev);
+    }
+}
+
