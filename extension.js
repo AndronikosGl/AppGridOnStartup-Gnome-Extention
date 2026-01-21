efab72dfcd5f63089d617969da89e7cdaf8bb6cd
AndronikosGl <anglaro@cs.duth.gr>
2026-01-21 22:11:36 +0200

    Refactor extension to use class-based structure

--- extension.js
+++ extension.js
@@ -1,39 +1,29 @@
-const Gio = imports.gi.Gio;
-const Main = imports.ui.main;
-
-let signalId = null;
-const settings = new Gio.Settings({ schema: 'org.gnome.desktop.interface' });
-
-function enable() {
-    // If GNOME is already running, do nothing
-    if (!Main.layoutManager._startingUp) return;
-
-    // Connect to startup-complete signal
-    signalId = Main.layoutManager.connect('startup-complete', () => {
-        Main.layoutManager.disconnect(signalId);
-        signalId = null;
-        _openAppGrid();
-    });
-}
-
-function disable() {
-    if (signalId) {
-        Main.layoutManager.disconnect(signalId);
-        signalId = null;
+import Gio from 'gi://Gio';
+    
+    import * as Main from 'resource:///org/gnome/shell/ui/main.js';
+    
+    export default class AppGridOnStartup {
+    
+        enable() {
+            if (!Main.layoutManager._startingUp) return;
+        
+            this._settings = new Gio.Settings({ schema: 'org.gnome.desktop.interface' });
+            
+            Main.layoutManager.connectObject('startup-complete', () => {
+                Main.layoutManager.disconnect(signalId);
+                this.#openAppGrid();
+            }, this);
+        }
+        
+        disable() {
+            this._settings = null;
+            Main.layoutManager.disconnectObject(this);
+        }
+    
+        #openAppGrid() {
+            const prev = this._settings.get_boolean('enable-animations');
+            this._settings.set_boolean('enable-animations', false);
+            Main.overview.showApps();
+            this._settings.set_boolean('enable-animations', prev);
+        }
     }
-}
-
-function _openAppGrid() {
-    const prev = settings.get_boolean('enable-animations');
-    settings.set_boolean('enable-animations', false);
-
-    Main.overview.showApps();
-
-    settings.set_boolean('enable-animations', prev);
-}
-
-// Export the functions for GNOME Shell
-var extension = {
-    enable: enable,
-    disable: disable
-};
